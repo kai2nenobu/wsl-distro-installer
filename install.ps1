dee0b8162c3c8976cc007a25be603e15cd8fa8d1
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-12-21 12:41:49 +0900

    First install script for Ubuntu-18.04, Debian

---
+++ install.ps1
@@ -0,0 +1,39 @@
+Param(
+  [ValidateSet('Ubuntu-18.04', 'Debian')][string]$Distro
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+$InstallLocation = Join-Path ${env:LOCALAPPDATA} 'WSL'
+$Distros = @{
+  'Ubuntu-18.04' = @{
+    'Name' = 'Ubuntu-18.04'
+    'PackageUrl' = 'https://aka.ms/wsl-ubuntu-1804'
+    'Exe' = 'ubuntu1804.exe'
+  };
+  'Debian' = @{
+    'Name' = 'Debian'
+    'PackageUrl' = 'https://aka.ms/wsl-debian-gnulinux'
+    'Exe' = 'debian.exe'
+  };
+}
+
+$distroArchive = $Distros[$Distro]['Name'] + '.zip'
+$distroUrl = $Distros[$Distro]['PackageUrl']
+#$distroPath = Join-Path $InstallLocation $distroName
+$distroPath = Join-Path '.' $Distros[$Distro]['Name']
+$distroExe = Join-Path $distroPath $Distros[$Distro]['Exe']
+
+## Download a distro package
+Invoke-WebRequest -Uri $distroUrl -OutFile $distroArchive -UseBasicParsing
+
+## Extract
+Expand-Archive -Path $distroArchive -DestinationPath $distroPath -Force
+# TODO Abort when the distro is already installed
+
+## Install
+Start-Process -FilePath $distroExe -ArgumentList install,--root -NoNewWindow -Wait
+
+## TODO Add a distro exe to PATH
+## TODO Set default distro if required
